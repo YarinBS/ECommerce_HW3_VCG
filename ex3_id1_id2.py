b479fa8da620659398592719288cb7bd74e12f60
Roi <thedarkhermit@gmail.com>
2021-06-26 12:19:12 +0300

    A B done

--- ex3_id1_id2.py
+++ ex3_id1_id2.py
@@ -1,5 +1,4 @@
 import pandas as pd
-import math
 from itertools import permutations
 
 
@@ -7,13 +6,47 @@ from itertools import permutations
 
 
 def opt_bnd(data, k, years):
-    # returns the optimal bundle of cars for that k and list of years and their total cost.
-    return {"cost": 0, "bundle": []}
+    cars = ['ford', 'bmw', 'kia', 'vw', 'ferrari']
+    total_cost = 0
+    winning_cars = []
+    for index in range(k):
+        lowest_v = 1000000000000
+        lowest_index_lst = []
+        permut = permutations(years, 5)
+        for p in permut:
+            v = 0
+            index_lst = []
+            gotNewPern = True
+            for index, car in enumerate(cars):
+                data_car = data[(data.brand == car) & (data.year == p[index])]
+                min_price = data_car['value'].min()
+                if pd.isnull(min_price):
+                    gotNewPern = False
+                    break
+                index_lst.append(data_car['value'].idxmin(axis=1))
+                v += min_price
+            if v < lowest_v and gotNewPern:
+                lowest_v = v
+                lowest_index_lst = index_lst
+
+        total_cost += lowest_v
+        winning_cars.extend(list(data.loc[lowest_index_lst, :]['id']))
+        data.drop(lowest_index_lst, inplace=True)
+        data.reset_index(inplace=True, drop=True)
+
+    return {"cost": total_cost, "bundle": winning_cars}
 
 
 def proc_vcg(data, k, years):
-    # runs the VCG procurement auction
-    return {'id': 0}
+    cars_dict = {}
+    data_copy = data.copy(deep=True)
+    bundle = opt_bnd(data_copy, k, years)
+    cost = bundle['cost']
+    cars = bundle['bundle']
+    for car in cars:
+        cars_dict[car] = abs((cost - int(data.loc[data['id'] == car, 'value'])) -
+                             opt_bnd(data.drop(data[data.id == car].index), k, years)['cost'])
+    return cars_dict
 
 
 ########## Part B ###############
@@ -37,7 +70,7 @@ def helper(sums: list, xs: list):
         return sums
 
     s = sums[len(sums) - 1]
-    sums.append(s + xs[0])
+    sums.append([xs[0][0], s[1] + xs[0][1]])
 
     if len(xs) > 1:
         return helper(sums, xs[1:])
@@ -55,40 +88,61 @@ class Type:
     def avg_buy(self) -> float:
         # runs a procurement vcg auction for buying cars_num cars on the given self.data.
         # returns the average price paid for a winning car.
-
-        auction_results = proc_vcg(self.data, self.cars_num, self.data[['year']])
-        price = 0
-        for value in auction_results.values():
-            price += value
-        return price / len(auction_results)
+        prices = list(self.data["value"])
+        return sorted(prices)[self.cars_num]
 
     def cdf(self, x):
         # return F(x) for the histogram self.data
         hist = {}
+        counter = 0
         for i in self.data["value"]:
             hist[i] = hist.get(i, 0) + 1
-        if hist.get(x) is None:
-            return 0
-        # using numpy np.cumsum to calculate the CDF
-        # We can also find using the PDF values by looping and adding
-        data = [[k, v] for k, v in hist.items()]  # use dic.items() for python3
+            counter = counter + 1
+        data = [[k, v / counter] for k, v in hist.items()]
         data.sort(reverse=False)
-        cdf = cumsum([1,2,3])
-        print(cdf)
+        cdf = cumsum(data)
+        Xvals = [str(item[0]) for item in cdf]
+        if x < int(Xvals[0]):
+            return 0
+        if x >= int(Xvals[-1]):
+            return 1
+        lb, ub = self.find_prev_con(x, Xvals)
+        return Fx(x, lb, ub, cdf)
 
-        return hist[x]
+    def find_prev_con(self, x, A):
+        for i in range(1, len(A)):
+            if int(A[i - 1]) <= x <= int(A[i]):
+                return int(A[i - 1]), int(A[i])
 
     def os_cdf(self, r, n, x):
         # The r out of n order statistic CDF
-        return 1
+        sum = 0
+        for j in range(r, n + 1):
+            FX = self.cdf(x)
+            sum += (comb(n, j) * (FX ** j) * ((1 - FX) ** (n - j)))
+        return sum
 
     def exp_rev(self):
         # returns the expected revenue in future auction for cars_num items and buyers_num buyers
+        n_minus_k_statistic_num = self.buyers_num - self.cars_num
+        max_price = max(list(self.data["value"]))
+        tail_func = sum(
+            [1 - self.os_cdf(n_minus_k_statistic_num, self.buyers_num, x) for x in range(max_price, -1, -1)])
+        return self.cars_num * tail_func
 
-        return 0
+    def find_median(self, lst):
+        n = len(lst)
+        s = sorted(lst)
+        return (sum(s[n // 2 - 1:n // 2 + 1]) / 2.0, s[n // 2])[n % 2] if n else None
 
     def exp_rev_median(self, n):
-        return 0
+        value_list = list(self.data["value"])
+        med_price = self.find_median(value_list)
+        prob = self.cdf(med_price)
+        expectancy = n * (prob ** (n - 1)) * (1 - prob) * med_price + sum(
+            [1 - self.os_cdf(n - 1, n, med_price) for index in range(med_price)]) + sum(
+            [1 - self.os_cdf(n - 1, n, p) for p in range(med_price, max(value_list) + 1)])
+        return expectancy
 
     ########## Part C ###############
 
--- main.py
+++ main.py
@@ -3,24 +3,30 @@ import ex3_id1_id2 as st
 
 data = pd.read_csv('Ex3_data.csv')
 
-######### Part A ##################
-k=4
-years = list(range(2012,2017))
-# outcome = st.comb_vcg(data,k,years)
+def main():
+    ######### Part A ##################
+    k=4
+    years = list(range(2012,2017))
+    # outcome = st.comb_vcg(data,k,years)
 
-########## Part B ##################
-type = st.Type("vw",2015,1700,data)
-type.cars_num = 20
-type.buyers_num = 100
-lala= type.cdf(8300)
-print('You achieved an expected average profit of', int((type.exp_rev()/type.cars_num)-type.avg_buy()), 'per car')
-type.cars_num = 1
-type.buyers_num = 2
-print('expected revenue in a one car auction with two buyers:', type.exp_rev())
-print('Adding a median reserve price makes it', type.exp_rev_median(2))
-print('And with a third buyer that is', type.exp_rev_median(3))
+    ########## Part B ##################
+    type = st.Type("vw",2015,1700,data)
+    type.cars_num = 20
+    type.buyers_num = 100
+    # lala= type.cdf(12000)
+    # print(lala)
+    # type.os_cdf(80, 100, 15000)
+    # print(type.avg_buy())
+    print('You achieved an expected average profit of', int((type.exp_rev()/type.cars_num)-type.avg_buy()), 'per car')
+    type.cars_num = 1
+    type.buyers_num = 2
+    print('expected revenue in a one car auction with two buyers:', type.exp_rev())
+    print('Adding a median reserve price makes it', type.exp_rev_median(2))
+    print('And with a third buyer that is', type.exp_rev_median(3))
 
 
+if __name__ == "__main__":
+    main()
 
 
 
